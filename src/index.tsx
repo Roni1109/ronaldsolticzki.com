11c3fabb31b62827fa4a3fd2e24e27998f762851
Horotan Gelu <horotangelu17@gmail.com>
2022-09-30 16:38:37 +0300

    Fixing ts errors

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root')!)
 
 root.render(
   <BrowserRouter>
