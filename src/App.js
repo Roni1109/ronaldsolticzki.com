0a12fdb4de3eea7d01a1742a47000dc585ce2a70
Horotan Gelu <horotangelu17@gmail.com>
2022-09-30 16:17:14 +0300

    eslint setup

--- src/App.js
+++
@@ -1,8 +0,0 @@
-const App = () => {
-  return <div className='App'>TEST
-  <h1>Hello</h1>
-  <button>Submit</button>
-  </div>;
-};
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      TEST
+      <h1>Hello</h1>
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default App
