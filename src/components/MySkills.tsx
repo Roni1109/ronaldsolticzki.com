a0a47509cb22529efdd180abe856a4824e6cda67
Solticzki Ronald <rsolticzki@gmail.com>
2023-08-31 23:34:22 +0300

    restyling: replaced PNGs with SVGs

--- src/components/MySkills.tsx
+++ src/components/MySkills.tsx
@@ -74,7 +74,7 @@ const MySkills = () => {
         />
         <img src={
           "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
-          } 
+        }
           alt='Logo' />
       </Styled_Grid>
     </Styled_Wrapper>
